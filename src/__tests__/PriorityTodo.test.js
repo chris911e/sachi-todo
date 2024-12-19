6ffa245f20457af95fabaf6469faafc9002e817d
Christopher Scheel <christopherscheel99@gmail.com>
2024-12-19 10:56:46 +0100

    priority test

---
+++ src/__tests__/PriorityTodo.test.js
@@ -0,0 +1,45 @@
+import { render, fireEvent } from "@testing-library/react";
+import TodoItem from "@/app/components/TodoItem";
+
+const todo = {
+    id: 1,
+    title: 'Test Todo',
+    completed: false,
+    category: "Unkategorisiert",
+    priority: 0,
+    expiration: new Date().toISOString()
+};
+
+
+const categories = [
+    "Unkategorisiert",
+    "Arbeit",
+    "Personal",
+    "Einkauf"
+]
+
+
+describe("Tests Priority Todo", () => {
+    test('should test priority todo', () => {
+        const screen = render(
+            <TodoItem
+                key={todo.id}
+                todo={todo}
+                selectedCategory={todo.category}
+                handleChangeProps={() => {}}
+                handleToDoCategoryChange={() => {}}
+                handleToDoPriorityChange={() => {}}
+                deleteTodoProps={() => {}}
+                setUpdate={() => {}}
+                setExpirationUpdate={() => {}}
+                categories={categories}
+              />
+        )
+        const priority = screen.getByTestId("priority-todo-1")
+        expect(priority).toBeInTheDocument()
+        fireEvent.click(priority)
+        
+        const priorityColor =getComputedStyle(priority).backgroundColor
+        expect(priorityColor).toBe("RoyalBlue")
+    })
+})
